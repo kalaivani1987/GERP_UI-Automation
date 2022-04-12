957d88cd7a17678b37687e43299523281467e1e3
kalaivani1987 <103469789+kalaivani1987@users.noreply.github.com>
2022-04-11 21:42:33 -0500

    Add files via upload
    I completed the Assignment 1And 2 third one hard for i need some help

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile Nokia = new Mobile();
+		Nokia.makeCall();
+		Nokia.sendmsg();
+		}
+	public void makeCall() {
+		System.out.println("This is makecall method");
+	}
+	public void sendmsg() {
+		System.out.println("This is send msg method");
+	}
+	
+	}
+
+
---
+++ mobile1.java
@@ -0,0 +1,21 @@
+package assinment2;
+
+public class mobile1 {
+public static String mobileModel = "1001";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  mobile1 Nokia =new mobile1();
+  System.out.println(mobileModel);
+  int weight=12;
+  boolean a1 =true;
+  boolean a2 =false;
+  double cost=120.50;
+  System.out.println(a1);
+  System.out.println(a2);
+  System.out.println(weight);
+  System.out.println(cost);
+	}
+	
+
+}
